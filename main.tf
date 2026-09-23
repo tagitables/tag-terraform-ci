ee72dca76bcb5db5f818c669bbd980ac8e543976
tagitables <quahkaixin@gmail.com>
2026-09-23 16:16:43 +0800

    fixed Terraform lint requirements

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-tfstate-ce13"
     key    = "tag/devsecops-3.2/terraform.tfstate"
